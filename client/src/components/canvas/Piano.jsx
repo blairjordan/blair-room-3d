2e0dd63f6e6ecaec06ec1624dc276ea3b24f0ddd
Blair Jordan <blair.j.jordan@gmail.com>
2024-01-28 03:50:55 +0800

    chore: more piano biz

--- client/src/components/canvas/Piano.jsx
+++ client/src/components/canvas/Piano.jsx
@@ -1,18 +1,69 @@
-import React, { useEffect, useContext, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { useThree } from '@react-three/fiber'
+import { useCamera } from '@react-three/drei'
 
 export function Piano({ scene }) {
   const { camera } = useThree()
+
+  useEffect(() => {
+    const originalKeyMeshes = scene.children.filter(
+      (child) => child.name.startsWith('piano_key_') || child.name.includes('_black_key'),
+    )
+
+    originalKeyMeshes.forEach((originalKeyMesh) => {
+      scene.remove(originalKeyMesh)
+    })
+  }, [scene])
+
+  const clonedKeyMeshes = scene.children
+    .filter((child) => child.name.startsWith('piano_key_') || child.name.includes('_black_key'))
+    .map((keyMesh, i) => {
+      console.log('Position:', keyMesh.position)
+      console.log('Rotation:', keyMesh.rotation)
+      console.log('Scale:', keyMesh.scale)
+
+      const keyName = `key_${21 + i}`
+      const isBlackKey = keyMesh.name.includes('black')
+      const newMaterial = new THREE.MeshBasicMaterial({ color: isBlackKey ? 0x000000 : 0xffffff })
+      const geometry = keyMesh.geometry.clone()
+      const position = keyMesh.position.clone()
+      const scale = keyMesh.scale.clone()
+      const rotation = keyMesh.rotation.clone()
+      const mesh = new THREE.Mesh(geometry, newMaterial)
+      const quaternion = keyMesh.quaternion.clone()
+
+      mesh.applyMatrix4(keyMesh.matrixWorld)
+      mesh.scale.copy(scale)
+      mesh.position.copy(position)
+      mesh.rotation.copy(rotation)
+      mesh.quaternion.copy(quaternion)
+
+      return {
+        keyName,
+        isPressed: false,
+        newMaterial,
+        geometry,
+        position,
+        scale,
+        rotation,
+        mesh,
+        quaternion,
+      }
+    })
 
   const audioLoader = useRef(new THREE.AudioLoader())
   const listener = useRef(new THREE.AudioListener())
   const sound = useRef(new THREE.Audio(listener.current))
 
   const playSound = (keyName) => {
-    // Assuming the naming convention "piano_key_XX.mp3" for audio files
-    const audioFileName = `piano_key_${keyName}.mp3`
+    const audioFileName = `piano/${keyName}.mp3`
 
+    if (sound.current.isPlaying) {
+      sound.current.stop()
+    }
+
+    console.log('🎵 playing', audioFileName)
     audioLoader.current.load(
       audioFileName,
       (buffer) => {
@@ -28,34 +79,23 @@ export function Piano({ scene }) {
     )
   }
 
-  // Mouse click event handler
-  const onMouseClick = (event) => {
-    event.preventDefault()
-
-    const mouse = new THREE.Vector2(
-      (event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1,
-    )
-
-    const raycaster = new THREE.Raycaster()
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObjects(scene.children)
-    if (intersects.length > 0) {
-      const clickedObject = intersects[0].object
-      if (clickedObject.name.startsWith('piano_key_')) {
-        playSound(clickedObject.name.split('_').pop())
-      }
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('click', onMouseClick)
-
-    return () => {
-      document.removeEventListener('click', onMouseClick)
-    }
-  }, [])
-
-  return null
+  return (
+    <>
+      {clonedKeyMeshes.map(({ keyName, geometry, scale, position, rotation, mesh, quaternion }) => {
+        return (
+          <mesh
+            key={keyName}
+            raycast={useCamera(camera)}
+            onPointerDown={() => playSound(keyName)}
+            geometry={geometry}
+            scale={scale}
+            rotation={rotation}
+            position={position}
+            material={mesh.material}
+            quaternion={quaternion}
+          ></mesh>
+        )
+      })}
+    </>
+  )
 }
